479ec8dc5155e1d784d1b58a975cfe427e30db69
pvq2k2 <75900733+pvq2k2@users.noreply.github.com>
2022-08-01 17:43:45 +0700

    Update products.js

--- src/routers/products.js
+++ src/routers/products.js
@@ -3,9 +3,9 @@ import { create, get, list, remove, update, getViaSlug } from '../controllers/pr
 
 const router = Router();
 
-router.get('/products', list);
+router.get('/product', list);
 router.get('/product/:slug', getViaSlug);
-router.get('/products/:id', get);
+router.get('/product/:id', get);
 router.post('/product', create);
 router.delete("/product/:id", remove);
 router.put("/product/:id", update);
