776cc16867fab473556d181d769c377db4a3e56f
pvq2k2 <vanquyet2392002e@gmail.com>
2022-08-02 22:17:06 +0700

    BACK-12 feature: Create controller user

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,10 +1,9 @@
 import { Router } from 'express';
-import { create, get, list, remove, update, getViaSlug } from '../controllers/users';
+import { create, get, list, remove, update } from '../controllers/users';
 
 const router = Router();
 
 router.get('/user', list);
-router.get('/user/:slug', getViaSlug);
 router.get('/user/:id', get);
 router.post('/user', create);
 router.delete("/user/:id", remove);
