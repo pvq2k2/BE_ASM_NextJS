e05287e1f4ba9f869c2e75d6601d606d9a05cf7f
pvq2k2 <75900733+pvq2k2@users.noreply.github.com>
2022-09-27 21:36:47 +0700

    Update auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -29,12 +29,12 @@ export const signin = async (req, res) => {
     const {email, password } = req.body;
     const user = await User.findOne({email}).exec();
     if (!user) {
-        res.status(401).json({
+        res.json({
             message: "User không tồn tại"
         })
     }
     if (!user.authenticate(password)) {
-        res.status(401).json({
+        res.json({
             message: "Mật khẩu không đúng"
         })
     }
